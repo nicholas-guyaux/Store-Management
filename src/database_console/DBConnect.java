b58b7ec3a107248ff16489adcda684985be05041
Nicholas Guyaux <nicholas.guyaux@gmail.com>
2017-02-21 08:46:04 -0700

    added documentation

--- src/database_console/DBConnect.java
+++ src/database_console/DBConnect.java
@@ -1,5 +1,24 @@
 package database_console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class DBConnect {
 
+		public static void main(String[] args)
+		{
+			try {
+				String host = "jdbc:derby://localhost:1527/Employees";
+				String uName = "username";
+				String uPass = "password";
+			
+				Connection con = DriverManager.getConnection( host, uName, uPass );
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+			
+			
+		}
 }
