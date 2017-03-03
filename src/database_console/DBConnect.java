129b669d2300cfdffdb21683e40b552a3b65a3d1
Nicholas Guyaux <nicholas.guyaux@gmail.com>
2017-03-03 10:35:54 -0700

    changed some data

--- src/database_console/DBConnect.java
+++ src/database_console/DBConnect.java
@@ -2,16 +2,10 @@ package database_console;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class DBConnect {
-	
-	
-	
 	
 
 	public static Connection getDBConnection() throws ClassNotFoundException, SQLException {
